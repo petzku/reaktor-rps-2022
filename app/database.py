1cfdd9b4b49421bf47d91bb68c7696743cce1a90
petzku <petzku@zku.fi>
2022-01-23 16:04:38 +0200

    Add database lookups for history

--- app/database.py
+++ app/database.py
@@ -3,12 +3,14 @@
 import sqlite3
 from uuid import uuid4
 import rps
+import math
 
 from typing import Optional
 from apityping import APIGameResult, GameResult, APIGameBegin, GameBegin, PlayerName, PlayerId
 
 
 DB_FILE = 'results.db'
+GAMES_PAGE_LENGTH = 50
 
 def get_last_history_page() -> Optional[str]:
     """ Returns latest unfetched history page address """
@@ -128,10 +130,105 @@ def add_history_games(data: list[APIGameResult]) -> None: # TODO: typing
     ids = get_or_create_players(list(names))
 
     # Preprocess results into nicer data format and save to database
+    # TODO: consolidate into one bigger operation, instead of separate insert per game?
     for api_res in data:
         res = result_from_api_result(api_res)
         if not add_game_result(res):
             print("Error adding game: ", game)
+
+
+def get_games_by_player(uuid: PlayerId, page: int) -> list[GameResult]:
+    """ Get nth page of a player's games. """
+    con = sqlite3.connect(DB_FILE)
+    cur = con.cursor()
+
+    query = """SELECT games.game_id, games.time, p1_id, p1.name, r1.played, r1.result, p2_id, p2.name, p2.played, p2.result
+        FROM games
+        INNER JOIN players AS p1 ON games.p1_id = p1.player_id
+        INNER JOIN players AS p2 ON games.p2_id = p2.player_id
+        INNER JOIN plays AS r1 ON games.game_id = r1.game_id AND games.p1_id = r1.player_id
+        INNER JOIN plays AS r2 ON games.game_id = r2.game_id AND games.p2_id = r2.player_id
+        ORDER BY games.time DESC
+        LIMIT :lim OFFSET :off
+        WHERE games.p1_id = :pid OR games.p2_id = :pid """
+    cur.execute(query, {'pid': uuid, 'lim': GAMES_PAGE_LENGTH, 'off': page*GAMES_PAGE_LENGTH})
+
+    return [
+        {
+            'gameId': gid,
+            't': t,
+            'player1': {
+                'pid': p1_id,
+                'name': p1_name,
+                'played': rps.rps_from_str(p1_play),
+                'result': rps.result_from_str(p1_res)
+            },
+            'player2': {
+                'pid': p2_id,
+                'name': p2_name,
+                'played': rps.rps_from_str(p2_play),
+                'result': rps.result_from_str(p2_res)
+            }
+        }
+        for gid, t, p1_id, p1_name, p1_play, p1_res, p2_id, p2_name, p2_play, p2_res in cur.fetchall()
+    ]
+
+def get_games_history(page: int) -> list[GameResult]:
+    """ Get nth page of all played games. """
+    con = sqlite3.connect(DB_FILE)
+    cur = con.cursor()
+
+    query = """SELECT games.game_id, games.time, p1_id, p1.name, r1.played, r1.result, p2_id, p2.name, p2.played, p2.result
+        FROM games
+        INNER JOIN players AS p1 ON games.p1_id = p1.player_id
+        INNER JOIN players AS p2 ON games.p2_id = p2.player_id
+        INNER JOIN plays AS r1 ON games.game_id = r1.game_id AND games.p1_id = r1.player_id
+        INNER JOIN plays AS r2 ON games.game_id = r2.game_id AND games.p2_id = r2.player_id
+        ORDER BY games.time DESC
+        LIMIT :lim OFFSET :off """
+    cur.execute(query, {'lim': GAMES_PAGE_LENGTH, 'off': page*GAMES_PAGE_LENGTH})
+
+    return [
+        {
+            'gameId': gid,
+            't': t,
+            'player1': {
+                'pid': p1_id,
+                'name': p1_name,
+                'played': rps.rps_from_str(p1_play),
+                'result': rps.result_from_str(p1_res)
+            },
+            'player2': {
+                'pid': p2_id,
+                'name': p2_name,
+                'played': rps.rps_from_str(p2_play),
+                'result': rps.result_from_str(p2_res)
+            }
+        }
+        for gid, t, p1_id, p1_name, p1_play, p1_res, p2_id, p2_name, p2_play, p2_res in cur.fetchall()
+    ]
+
+def get_games_count_by_player(uuid: PlayerId) -> tuple[int, int]:
+    """ Get count of games and pages for player """
+
+    con = sqlite3.connect(DB_FILE)
+    cur = con.cursor()
+
+    cur.execute("SELECT COUNT(*) FROM games WHERE p1_id = ? OR p2_id = ?", (uuid, uuid))
+
+    (n,) = cur.fetchone()
+    return n, math.ceil(n / GAMES_PAGE_LENGTH)
+
+def get_games_count_total() -> tuple[int, int]:
+    """ Get total count of games and pages """
+
+    con = sqlite3.connect(DB_FILE)
+    cur = con.cursor()
+
+    cur.execute("SELECT COUNT(*) FROM games")
+
+    (n,) = cur.fetchone()
+    return n, math.ceil(n / GAMES_PAGE_LENGTH)
 
 
 def result_from_api_result(api_res: APIGameResult) -> GameResult:
