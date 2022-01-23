b3a82bdfe493914c69435dd487b2e6dcae8736fd
petzku <petzku@zku.fi>
2022-01-23 22:56:33 +0200

    Add live updating of games
    Untested (for now)

--- app/static/sockets.js
+++ app/static/sockets.js
@@ -15,4 +15,26 @@ socket.on('game result', function(data) {
     console.debug("result!");
     var gameId = data.gameId;
     console.debug(gameId);
+    remove_finished(gameId)
 });
+
+function add_live(gameInfo) {
+    var left_link = $("<a>", {href: '/player/'+gameInfo.player1.pid}).text(gameInfo.player1.name)
+    var left = $("<div>", {class: 'player left'}).append(left_link);
+
+    var right_link = $("<a>", {href: '/player/'+gameInfo.player2.pid}).text(gameInfo.player2.name)
+    var right = $("<div>", {class: 'player right'}).append(right_link);
+    
+    var middle = $("<span>", {class: 'middle'}).text("vs.");
+    var game_li = $("<li>", {class: 'result', 'id': gameInfo.gameId}).append(left).append(middle).append(right);
+
+    $("#live").prepend(game_li);
+    $("#nolive").hide();
+}
+
+function remove_finished(gameId) {
+    $("#live > li#"+gameId).remove();
+    if ($("#live > li").length === 0) {
+        $("#nolive").show();
+    }
+}
